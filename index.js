db06d16606f3a3d5cf811e3cdaab0786d4e1f2e4
Wolfik-masha-html <sinatinskamaria@gmail.com>
2023-11-25 18:28:25 +0200

    Game

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const timeList = document.querySelector(".time-list");
 const reload = document.querySelector(".game-new");
 const board = document.querySelector(".board");
 const timeEl = document.querySelector("#time");
+
 let time = 0;
+let score = 0;
 
 console.log(reload);
 startBtn.addEventListener("click", () => {
@@ -20,8 +22,17 @@ timeList.addEventListener("click", (e) => {
     }
   });
 
+  board.addEventListener("click", (e) => {
+    if (e.target.classList.contains("circle")){
+      score++;
+      e.target.remove();
+      createRandomCircle();
+    }
+  })
+
 function startGame() {
   setInterval(decreaseTime, 1000);
+  createRandomCircle()
   setTime(time);
 }
 
@@ -40,8 +51,27 @@ function setTime(value) {
   timeEl.innerHTML = `00:${value}`;
 }
 
+function getRandomNumber(min, max){
+  return Math.round(Math.random() * (max - min) + min);
+};
+function createRandomCircle() {
+  const circle = document.createElement("div")
+  const size = getRandomNumber(20,80);
+  const { width, heigh } = board.getBoundingClientRect();
+  const x = getRandomNumber(0, width-size);
+  const y = getRandomNumber(0, heigh-size);
+
+  circle.classList.add("circle");
+  circle.style.width = `${size}px`;
+  circle.style.height = `${size}px`;
+  circle.style.top = `${y}px`;
+  circle.style.left = `${x}px`;
+
+  board.append(circle);
+}
 function finishGame(){
-    board.innerHTML = `<h3>your score</h3>`
+    board.innerHTML = `<h3>your score:${score}</h3>`
+    timeEl.parentNode.classList.add("hide")
 }
 reload.addEventListener("click", () => {
   location.reload();
